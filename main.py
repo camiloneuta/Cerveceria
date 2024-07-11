a968385dc73c16352bc3670df42150c4dd05eb98
camiloneuta <juancamiloneuta2512@gmail.com>
2024-07-11 12:26:01 -0500

    Se agrega el proyecto

---
+++ main.py
@@ -0,0 +1,10 @@
+print("=========================")
+print(" ")
+print("Bienvenido! \n¿No sabes que nombre colocarle a tu cervecería? "
+      "\nNo te preocupes, hoy te ayudaré con tu problema, solo tienes que responder dos preguntas.")
+print(" ")
+print("=========================")
+print(" ")
+print("El nombre de tu cervecería es: \n\t " + "\""
+      + input("¿Cuál es tu comida favorita?: ")
+      + input("¿Cuál es tu serie favorita?: ") + "\"")
